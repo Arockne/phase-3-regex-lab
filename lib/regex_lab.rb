20a43ced48bdfbe26f084ce2817230fd742a082a
Daniel Calvin <arockne2003@yahoo.com>
2021-10-01 11:04:21 -0700

    commit tree 7b5103556b51ad4a6512ecbf32d2255afa180626

--- lib/regex_lab.rb
+++ lib/regex_lab.rb
@@ -1,19 +1,19 @@
 def starts_with_a_vowel?(word)
-
+   word.match(/\b[aeiouAEIOU]\w+\b/) != nil
 end
 
 def words_starting_with_un_and_ending_with_ing(text)
-
+  text.scan(/un\w+ing/)
 end
 
 def words_five_letters_long(text)
-
+  text.scan(/\b\w{5}\b/)
 end
 
 def first_word_capitalized_and_ends_with_punctuation?(text)
-
+  text.match(/^[A-Z].+[.?!]$/) != nil
 end
 
 def valid_phone_number?(phone)
-
+  phone.match(/\b\(?\d{3}[)-]?[ ]?\d{3}-?[ ]?\d{4}\b/) != nil
 end
